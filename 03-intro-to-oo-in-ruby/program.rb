5e265fd10ad28f72bb6fcbcf7ab48c9b3b849084
joshua-miles <joshua@christetulit.org>
2018-10-10 13:27:01 -0500

    completed lecture code

--- 03-intro-to-oo-in-ruby/program.rb
+++ 03-intro-to-oo-in-ruby/program.rb
@@ -1,10 +1,69 @@
-hero_first_name = 'Steve'
+require 'pry'
 
-hero_last_name = 'Rodgers'
+class Hero 
 
-hero_alias = 'Captain America'
+    @@all = []
 
-hero_abilities = [
+    def initialize(first_name, last_name, abilities) 
+        @first_name = first_name
+        @last_name = last_name
+        @abilities = abilities
+        @@all << self
+    end
+
+    attr_accessor :first_name
+    # attr_reader :first_name - Only Reader Access
+    # attr_writer :first_name - Only Writer Access
+    # def first_name
+    #     @first_name
+    # end
+
+    # def first_name=(value)
+    #     @first_name = value
+    # end
+
+    attr_accessor :last_name
+    # def last_name
+    #     @last_name
+    # end
+
+    # def last_name=(value)
+    #     @last_name = value
+    # end
+
+    def full_name
+        binding.pry
+        @first_name + ' ' + @last_name
+    end
+
+   attr_reader :abilities
+
+    def coolest_ability
+        coolest_level_found = -Float::INFINITY
+        coolest_ability_found = nil
+        @abilities.each do | ability |
+            if ability[:coolness] > coolest_level_found 
+                coolest_level_found = ability[:coolness]
+                coolest_ability_found = ability
+            end
+        end
+        coolest_ability_found
+    end
+
+    def self.with_most_abilities
+        current_highest_ability_count = @@all[0].abilities.length
+        current_highest_ability_hero = @@all[0]
+        @@all.each do | hero |
+            if hero.abilities.length > current_highest_ability_count
+                current_highest_ability_count = hero.abilities.length
+                current_highest_ability_hero = hero
+            end
+        end
+        current_highest_ability_hero
+    end
+end
+
+steve = Hero.new('Steve', 'Rodgers', [
     {
         coolness: 5,
         name: 'Super Strength'
@@ -13,8 +72,18 @@ hero_abilities = [
         coolness: 10,
         name: 'Gravity Defying Shield'
     }
-]
+])
 
-def hero_full_name
-    hero_first_name + ' ' + hero_last_name
-end
+
+
+thor = Hero.new('Thor', 'Odinson', [
+    {
+        name: 'Lightening',
+        coolness: 10
+    }
+])
+
+Hero.with_most_abilities
+binding.pry
+# steve.coolest_ability
+0
