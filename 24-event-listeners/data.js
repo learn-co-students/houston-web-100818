472d7a65fa1b42277188a011b4a2c25d9b1d3182
joshua-miles <joshua@christetulit.org>
2018-11-21 13:55:39 -0600

    added event listener code

---
+++ 24-event-listeners/data.js
@@ -0,0 +1,39 @@
+const data = {
+    "Dogs": [
+        {
+            name: 'Perdita',
+            breed: 'Dalmation',
+            image: 'https://vignette.wikia.nocookie.net/disney/images/2/23/Tumblr_n03ddfk1Uf1r3jmn6o2_500.png/revision/latest?cb=20140129042356'
+        },
+        {
+            name:'Pongo',
+            breed: 'Dalmation',
+            image: 'https://vignette.wikia.nocookie.net/p__/images/1/1f/Pongo_smiling_sweetly.png/revision/latest?cb=20130902234230&path-prefix=protagonist'
+        },
+        {
+            name: 'Lady',
+            breed: 'Collie',
+            image: 'https://vignette.wikia.nocookie.net/ladyandtramp/images/2/23/Tramp-disneyscreencaps_com-1216.jpg/revision/latest?cb=20121108202046'
+        },
+        {
+            name: 'Tramp',
+            breed: 'Mut',
+            image: 'http://www.cornel1801.com/disney/Lady-Tramp-II-Scamps-Adventure-2001/characters/Tramp.jpg'
+        },
+        {
+            name: 'Ann',
+            breed: 'Coon Hound',
+            image:'http://rfern111.weebly.com/uploads/7/1/0/4/71046507/481812.jpg'
+        },
+        {
+            name: 'Dan',
+            breed: 'Coon Hound',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE7gyYbDm8v2xHenVgc2iXusKPgSK_jEptaAa_VSHZ15Tvdh_bPQ'
+        },
+        {
+            name:'Tess',
+            breed:'(Nay, tis Alan!)',
+            image:'https://avatars2.githubusercontent.com/u/13387775?s=460&v=4'
+        },
+    ]
+}
