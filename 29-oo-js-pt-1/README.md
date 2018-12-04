83466f31d7194f77e8be6399dcfa3f48547ca4be
joshua-miles <joshua@christetulit.org>
2018-12-04 11:22:15 -0600

    completed oo-js

---
+++ 29-oo-js-pt-1/README.md
@@ -0,0 +1,46 @@
+# Object Oriented JavaScript (Part I)
+
+## Vocabulary
+
+* Object
+* Factory Function
+* Constructor Function
+* `prototype`
+* `__proto__`
+* `class`
+* Inheritance
+* `extends`
+* `static`
+
+## Deliverables
+
+* Refactor our js-character to group our data and behavior into reuseable objects so we can create a second character
+
+## Outline
+
+* Introduction
+
+  * What is an object?
+
+  * Todays deliverables
+
+  * We will take a trip through JavaScripts history
+
+    |                         | Generator Functions | Constructor Functions | Class Syntax |
+    | ----------------------- | ------------------- | --------------------- | ------------ |
+    | Syntactically Appealing | Y                   | X                     | Y            |
+    | Performant              | X                   | Y                     | Y            |
+    | Compatible              | Y                   | Y                     | X            |
+
+* **Factory Functions**
+
+* **Constructor Functions**
+
+  * **`prototype`**
+  * **`proto`**
+
+* **Class Syntax**
+
+  * **Inheritance**
+  * **`extends`**
+  * **`static`**
--- 29-oo-js-pt-1/js-character/index.js
+++ 29-oo-js-pt-1/js-character/index.js
@@ -1,54 +1,180 @@
-
 const framesPerSecond = 60;
 
-const characterElement = document.createElement('img')
-const characterAssets = 'assets/character'
-characterElement.src = `${characterAssets}/static.gif`
-characterElement.style.position = 'absolute'
-characterElement.style.left = '0px'
-characterElement.style.top = '0px'
-characterElement.style.width = "50px"
-document.body.appendChild(characterElement)
+class Character {
 
-let movement;
+    constructor(sprites, speed){
+        this.element = document.createElement('img')
+        this.assets = `assets/${sprites}`
+        this.speed = speed
+        this.element.src = `${this.assets}/static.gif`
+        this.element.style.position = 'absolute'
+        this.element.style.left = '0px'
+        this.element.style.top = '0px'
+        this.element.style.width = "50px"
+        document.body.appendChild(this.element)
+        Character.allArray.push(this)
+    }
 
-function walkEast(){
-    stop()
-    movement = setInterval(function(){
-        let currentPosition = parseInt(characterElement.style.left)
-        characterElement.style.left = currentPosition + speed*1 + 'px'
-    }, 1000/framesPerSecond)  
-    characterElement.src = `${characterAssets}/walkright.gif`
+    walkEast(){
+        this.stop()
+        this.movement = setInterval( () => {
+            let currentPosition = parseInt(this.element.style.left)
+            this.element.style.left = currentPosition + this.speed + 'px'
+        }, 1000/framesPerSecond)  
+        this.element.src = `${this.assets}/walkright.gif`
+    }
+    
+    walkWest(){
+        this.stop()
+        this.movement = setInterval( () => {
+            let currentPosition = parseInt(this.element.style.left)
+            this.element.style.left = currentPosition - this.speed + 'px'
+        }, 1000/framesPerSecond)  
+        this.element.src = `${this.assets}/walkleft.gif`
+    }
+    
+    walkNorth(){
+        this.stop()
+        this.movement = setInterval( () => {
+            let currentPosition = parseInt(this.element.style.top)
+            this.element.style.top = currentPosition - this.speed + 'px'
+        }, 1000/framesPerSecond)  
+        this.element.src = `${this.assets}/walkup.gif`
+    }
+    
+    walkSouth(){
+        this.stop()
+        this.movement = setInterval( () => {
+            let currentPosition = parseInt(this.element.style.top)
+            this.element.style.top = currentPosition + this.speed + 'px'
+        }, 1000/framesPerSecond)  
+        this.element.src = `${this.assets}/walkdown.gif`
+    }
+    
+    stop(){
+        clearInterval(this.movement)
+        this.element.src = `${this.assets}/static.gif`
+    }
+
 }
 
-function walkWest(){
-    stop()
-    movement = setInterval(function(){
-        let currentPosition = parseInt(characterElement.style.left)
-        characterElement.style.left = currentPosition - speed*1 + 'px'
-    }, 1000/framesPerSecond)  
-    characterElement.src = `${characterAssets}/walkleft.gif`
-}
 
-function walkNorth(){
-    stop()
-    movement = setInterval(function(){
-        let currentPosition = parseInt(characterElement.style.top)
-        characterElement.style.top = currentPosition - speed*1 + 'px'
-    }, 1000/framesPerSecond)  
-    characterElement.src = `${characterAssets}/walkup.gif`
-}
+// function Character(){
+//     this.element = document.createElement('img')
+//     this.assets = 'assets/character'
+//     this.speed = 2
+//     this.element.src = `${this.assets}/static.gif`
+//     this.element.style.position = 'absolute'
+//     this.element.style.left = '0px'
+//     this.element.style.top = '0px'
+//     this.element.style.width = "50px"
+//     document.body.appendChild(this.element)
+// }
 
-function walkSouth(){
-    stop()
-    movement = setInterval(function(){
-        let currentPosition = parseInt(characterElement.style.top)
-        characterElement.style.top = currentPosition + speed*1 + 'px'
-    }, 1000/framesPerSecond)  
-    characterElement.src = `${characterAssets}/walkdown.gif`
-}
+// Character.prototype.walkEast = function(){
+//     this.stop()
+//     this.movement = setInterval( () => {
+//         let currentPosition = parseInt(this.element.style.left)
+//         this.element.style.left = currentPosition + this.speed + 'px'
+//     }, 1000/framesPerSecond)  
+//     this.element.src = `${this.assets}/walkright.gif`
+// }
 
-function stop(){
-    clearInterval(movement)
-    characterElement.src = `${characterAssets}/static.gif`
-}
+// Character.prototype.walkWest = function(){
+//     this.stop()
+//     this.movement = setInterval( () => {
+//         let currentPosition = parseInt(this.element.style.left)
+//         this.element.style.left = currentPosition - this.speed + 'px'
+//     }, 1000/framesPerSecond)  
+//     this.element.src = `${this.assets}/walkleft.gif`
+// }
+
+// Character.prototype.walkNorth = function(){
+//     this.stop()
+//     this.movement = setInterval( () => {
+//         let currentPosition = parseInt(this.element.style.top)
+//         this.element.style.top = currentPosition - this.speed + 'px'
+//     }, 1000/framesPerSecond)  
+//     this.element.src = `${this.assets}/walkup.gif`
+// }
+
+// Character.prototype.walkSouth = function(){
+//     this.stop()
+//     this.movement = setInterval( () => {
+//         let currentPosition = parseInt(this.element.style.top)
+//         this.element.style.top = currentPosition + this.speed + 'px'
+//     }, 1000/framesPerSecond)  
+//     this.element.src = `${this.assets}/walkdown.gif`
+// }
+
+// Character.prototype.stop = function(){
+//     clearInterval(this.movement)
+//     this.element.src = `${this.assets}/static.gif`
+// }
+
+
+
+
+// function createCharacter(){
+//     const character = {}
+
+//     character.element = document.createElement('img')
+//     // const characterElement = document.createElement('img')
+//     character.assets = 'assets/character'
+//     // const characterAssets = 'assets/character'
+//     character.speed = 2
+//     // const speed = 2
+//     character.element.src = `${character.assets}/static.gif`
+//     character.element.style.position = 'absolute'
+//     character.element.style.left = '0px'
+//     character.element.style.top = '0px'
+//     character.element.style.width = "50px"
+//     document.body.appendChild(character.element)
+
+//     // let movement
+//     // character.movement = null
+
+//     character.walkEast = function(){
+//         this.stop()
+//         character.movement = setInterval(function(){
+//             let currentPosition = parseInt(character.element.style.left)
+//             character.element.style.left = currentPosition + character.speed + 'px'
+//         }, 1000/framesPerSecond)  
+//         character.element.src = `${character.assets}/walkright.gif`
+//     }
+
+//     character.walkWest = function(){
+//         this.stop()
+//         character.movement = setInterval(function(){
+//             let currentPosition = parseInt(character.element.style.left)
+//             character.element.style.left = currentPosition - character.speed + 'px'
+//         }, 1000/framesPerSecond)  
+//         character.element.src = `${character.assets}/walkleft.gif`
+//     }
+
+//     character.walkNorth = function(){
+//         this.stop()
+//         character.movement = setInterval(function(){
+//             let currentPosition = parseInt(character.element.style.top)
+//             character.element.style.top = currentPosition - character.speed + 'px'
+//         }, 1000/framesPerSecond)  
+//         character.element.src = `${character.assets}/walkup.gif`
+//     }
+
+//     character.walkSouth = function(){
+//         this.stop()
+//         character.movement = setInterval(function(){
+//             let currentPosition = parseInt(character.element.style.top)
+//             character.element.style.top = currentPosition + character.speed + 'px'
+//         }, 1000/framesPerSecond)  
+//         character.element.src = `${character.assets}/walkdown.gif`
+//     }
+
+//     character.stop = function(){
+//         clearInterval(character.movement)
+//         character.element.src = `${character.assets}/static.gif`
+//     }
+
+//     return character
+
+// }
