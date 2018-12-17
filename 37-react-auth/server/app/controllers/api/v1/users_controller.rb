f25bcb6ec8ac81242cfbc130b54a188910fadd37
joshua-miles <joshua@christetulit.org>
2018-12-17 17:38:24 -0600

    initial

---
+++ 37-react-auth/server/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,42 @@
+class Api::v1::UsersController < ApplicationController
+    before_action :define_current_user
+    
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
+    
+    def index
+        render json: User.all
+    end
+    
+    def show
+        render json: current_user
+    end
+    
+    def update
+        current_user.update(user_params)
+        render json: current_user
+    end
+    
+    def destroy
+        current_user.destroy
+        render json: current_user
+    end
+    
+    def user_params
+        params.permit(:name, :email, :phone)
+    end
+    
+    def define_current_user
+        if params[:id]
+            @current_user = User.find(params[:id])
+        else
+            @current_user = User.new
+        end
+    end
+    
+    def current_user
+        @current_user
+    end
+end
